d4aa9bb9b44510e88dbe5e3029e194a8e0aa73ce
Upadhyay B <upadhyay.b@dgsl.in>
2024-07-21 12:01:14 +0530

    UI_UX

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,25 +1,32 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { auth, googleAuthProvider, signInWithPopup, signInWithEmailAndPassword, createUserWithEmailAndPassword } from '../firebase';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import {
+  auth,
+  googleAuthProvider,
+  signInWithPopup,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+} from "../firebase";
 
-const Profile = ({ setAuthenticated }) => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [isSignUp, setIsSignUp] = useState(false);
+const Profile = ({ setIsAuthenticated }) => {
   const navigate = useNavigate();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isSignUp, setIsSignUp] = useState(false);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
       if (isSignUp) {
         await createUserWithEmailAndPassword(auth, email, password);
-        alert('Sign up successful. You can now log in.');
+        alert("Sign up successful. You can now log in.");
         setIsSignUp(false);
       } else {
         await signInWithEmailAndPassword(auth, email, password);
-        sessionStorage.setItem('isAuthenticated', 'true');
-        setAuthenticated(true);
-        navigate('/products');
+        sessionStorage.setItem("isAuthenticated", "true");
+        setIsAuthenticated(true);
+        navigate("/products");
       }
     } catch (error) {
       alert(error.message);
@@ -29,18 +36,20 @@ const Profile = ({ setAuthenticated }) => {
   const handleGoogleLogin = async () => {
     try {
       await signInWithPopup(auth, googleAuthProvider);
-      sessionStorage.setItem('isAuthenticated', 'true');
-      setAuthenticated(true);
-      navigate('/products');
+      sessionStorage.setItem("isAuthenticated", "true");
+      setIsAuthenticated(true);
+      navigate("/products");
     } catch (error) {
-      console.error('Google sign-in error', error);
+      console.error("Google sign-in error", error);
     }
   };
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <div className="bg-white p-8 rounded shadow-md w-full max-w-md">
-        <h1 className="text-2xl font-bold mb-6 text-center">{isSignUp ? 'Sign Up for an account' : 'Login to your account'}</h1>
+        <h1 className="text-2xl font-bold mb-6 text-center">
+          {isSignUp ? "Sign Up for an account" : "Login to your account"}
+        </h1>
         <form onSubmit={handleSubmit} className="space-y-6">
           <div>
             <label className="block text-gray-700 mb-2">Email</label>
@@ -68,27 +77,33 @@ const Profile = ({ setAuthenticated }) => {
             type="submit"
             className="w-full bg-blue-500 text-white p-3 rounded hover:bg-blue-600 transition-colors"
           >
-            {isSignUp ? 'Sign Up' : 'Login'}
+            {isSignUp ? "Sign Up" : "Login"}
           </button>
         </form>
         <button
           onClick={handleGoogleLogin}
           className="w-full bg-red-500 text-white p-3 rounded hover:bg-red-600 transition-colors mt-4"
         >
-          {isSignUp ? 'Sign Up with Google' : 'Login with Google'}
+          {isSignUp ? "Sign Up with Google" : "Login with Google"}
         </button>
         <div className="text-center mt-4">
           {isSignUp ? (
             <p>
-              Already have an account?{' '}
-              <button onClick={() => setIsSignUp(false)} className="text-blue-500 hover:underline">
+              Already have an account?{" "}
+              <button
+                onClick={() => setIsSignUp(false)}
+                className="text-blue-500 hover:underline"
+              >
                 Sign in
               </button>
             </p>
           ) : (
             <p>
-              Don't have an account?{' '}
-              <button onClick={() => setIsSignUp(true)} className="text-blue-500 hover:underline">
+              Don't have an account?{" "}
+              <button
+                onClick={() => setIsSignUp(true)}
+                className="text-blue-500 hover:underline"
+              >
                 Sign up
               </button>
             </p>
--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -1,41 +1,69 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState } from "react";
 
 // Create a Context
 const CartContext = createContext();
 
 // Create a Provider Component
 const CartProvider = ({ children }) => {
-  const [cartItems, setCartItems] = useState([]);
-  const [cartCount, setCartCount] = useState(0);
+  console.log(JSON.parse(sessionStorage.getItem("cartItems")));
+  const [cartItems, setCartItems] = useState(
+    JSON.parse(sessionStorage.getItem("cartItems")) || []
+  );
+  const [cartCount, setCartCount] = useState(
+    JSON.parse(sessionStorage.getItem("cartCount")) || 0
+  );
 
   const handleAddToCart = (product) => {
-    const existingItem = cartItems.find(item => item.id === product.id);
+    const existingItem = cartItems.find((item) => item.id === product.id);
     if (existingItem) {
-      setCartItems(cartItems.map(item => item.id === product.id ? { ...item, count: item.count + 1 } : item));
+      const updatedCartItems = cartItems.map((item) =>
+        item.id === product.id ? { ...item, count: item.count + 1 } : item
+      );
+      setCartItems(updatedCartItems);
+      sessionStorage.setItem("cartItems", JSON.stringify(updatedCartItems));
     } else {
-      setCartItems([...cartItems, { ...product, count: 1 }]);
+      const updatedCartItems = [...cartItems, { ...product, count: 1 }];
+      setCartItems(updatedCartItems);
       setCartCount(cartCount + 1);
+      sessionStorage.setItem("cartCount", JSON.stringify(cartCount + 1));
+      sessionStorage.setItem("cartItems", JSON.stringify(updatedCartItems));
     }
   };
 
   const handleSubtractFromCart = (id) => {
-    const existingItem = cartItems.find(item => item.id === id);
+    const existingItem = cartItems.find((item) => item.id === id);
     if (existingItem.count === 1) {
-      setCartItems(cartItems.filter(item => item.id !== id));
+      const updatedCartItems = cartItems.filter((item) => item.id !== id);
+      setCartItems(updatedCartItems);
+      sessionStorage.setItem("cartItems", JSON.stringify(updatedCartItems));
       setCartCount(cartCount - 1);
+      sessionStorage.setItem("cartCount", JSON.stringify(cartCount - 1));
     } else {
-      setCartItems(cartItems.map(item => item.id === id ? { ...item, count: item.count - 1 } : item));
+      const updatedCartItems = cartItems.map((item) =>
+        item.id === id ? { ...item, count: item.count - 1 } : item
+      );
+      setCartItems(JSON.stringify(updatedCartItems));
     }
   };
 
   const handleRemoveItem = (id) => {
-    const itemToRemove = cartItems.find(item => item.id === id);
-    setCartItems(cartItems.filter(item => item.id !== id));
-    setCartCount(cartCount - itemToRemove.count);
+    const updatedCartItems = cartItems.filter((item) => item.id !== id);
+    setCartItems(updatedCartItems);
+    sessionStorage.setItem("cartItems", JSON.stringify(updatedCartItems));
+    setCartCount(cartCount - 1);
+    sessionStorage.setItem("cartCount", JSON.stringify(cartCount - 1));
   };
 
   return (
-    <CartContext.Provider value={{ cartItems, cartCount, handleAddToCart, handleSubtractFromCart, handleRemoveItem }}>
+    <CartContext.Provider
+      value={{
+        cartItems,
+        cartCount,
+        handleAddToCart,
+        handleSubtractFromCart,
+        handleRemoveItem,
+      }}
+    >
       {children}
     </CartContext.Provider>
   );
